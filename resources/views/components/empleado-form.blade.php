e6676cdb480494599008d2b0d326129b9367f6d8
Ferclemens <69605971+Ferclemens@users.noreply.github.com>
2022-12-11 21:12:05 -0300

    :speech_balloon: add confirm alerts to update and delete actions

--- resources/views/components/empleado-form.blade.php
+++ resources/views/components/empleado-form.blade.php
@@ -49,7 +49,22 @@
         <input type="number" name='sueldo' id='InputSueldo' class="form-control" placeholder="Ingrese Sueldo" value="{{ old('sueldo', $empleado->sueldo)}}">
     </div>
     <div class="col-sm-12 text-center mt-3">
-        <button type="submit" class="btn px-5 button__general">Guardar</button>
+        <button type="button" data-bs-toggle="modal" data-bs-target="#exampleModal" class="btn px-5 button__general">Guardar</button>
         <a href="{{ route('welcome')}}" class="btn button__general">Volver</a>
+    </div>
+</div>
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+        <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">¿Guardar los cambios?</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn button__general" data-bs-dismiss="modal">Cancelar</button>
+            <button type="submit" class="btn btn-success">Guardar</button>
+        </div>
+        </div>
     </div>
 </div>
--- resources/views/components/sucursal-form.blade.php
+++ resources/views/components/sucursal-form.blade.php
@@ -26,7 +26,22 @@
         </select>
     </div>
     <div class="col-sm-12 text-center mt-4">
-        <button type="submit" class="btn px-5 button__general">Guardar</button>
+        <button type="button" data-bs-toggle="modal" data-bs-target="#exampleModal" class="btn px-5 button__general">Guardar</button>
         <a href="{{ route('welcome')}}" class="btn button__general">Volver</a>
+    </div>
+</div>
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+        <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">¿Guardar los cambios?</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn button__general" data-bs-dismiss="modal">Cancelar</button>
+            <button type="submit" class="btn btn-success">Guardar</button>
+        </div>
+        </div>
     </div>
 </div>
